a54e56743d95a43ffff08bacfa065de2ec6ad00c
JJ Asghar <jj@chef.io>
2016-06-10 14:44:14 -0500

    removed some whitespace

--- bin/secrets.rb
+++ bin/secrets.rb
@@ -9,7 +9,7 @@ Dir.glob('jobs/*.yml') do |item|
   File.open(item) do |file|
     jjb_yml = file.readlines
     absolute_path = Pathname.new(File.expand_path(item))
-    file_name = absolute_path.basename   
+    file_name = absolute_path.basename
     output = File.open("jobs/temp/temp-#{file_name}", "w")
     jjb_yml.each do |line|
       output <<  line.gsub("auth-token: 'SLACK-AUTH-TOKEN'","auth-token: '#{secrets['slack-auth-token']}'" )
